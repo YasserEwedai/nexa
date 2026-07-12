37581c9932f6ac7fc8af9717ff0153acf3a10f56
Eng Kamal Abu Zarifa <144855748+kamalabuzarifa@users.noreply.github.com>
2026-07-12 13:48:38 +0300

    udate Logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -87,7 +87,7 @@ export default function Navbar() {
           {/* Logo */}
           <Link href="/" className="shrink-0">
             <Image
-              src="/public/images/logo.jpeg"
+              src="/images/logo.jpeg"
               alt="Nexa Team"
               width={150}
               height={45}
